8b6dbaf769d91f68498daf2f8030f6755910a366
Amna <amna.syed@stonybrook.edu>
2023-12-19 01:47:50 +0000

    api

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 import os
 from dotenv import load_dotenv
 import sentry_sdk
+from flask import Flask, jsonify, request
 
 sentry_sdk.init(
     dsn="https://127e735d2fff5cdf04c65db1d3bb20cd@o4506300835758080.ingest.sentry.io/4506408500199424",
@@ -65,5 +66,26 @@ def patients():
 def error():
     raise Exception('This is a test error for Sentry Testing')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+patients_data = [
+    {"id": "131313", "first_name": "Phillip", "last_name": "Duran", "date_of_birth": "1996-07-22", "gender": "Male"},
+    {"id": "878787", "first_name": "Alan", "last_name": "Brown", "date_of_birth": "1967-08-06", "gender": "Male"},
+    {"id": "101010", "first_name": "Mark", "last_name": "Dixon", "date_of_birth": "1980-12-22", "gender": "Male"},
+    {"id": "181818", "first_name": "Renee", "last_name": "Garcia", "date_of_birth": "2021-03-28", "gender": "Female"},
+    {"id": "636363", "first_name": "Joshua", "last_name": "Holmes", "date_of_birth": "1962-08-19", "gender": "Male"},
+    {"id": "123456", "first_name": "Daniel", "last_name": "Rivera", "date_of_birth": "2016-10-26", "gender": "Male"},
+    {"id": "127464", "first_name": "Rita", "last_name": "Perry", "date_of_birth": "1999-06-17", "gender": "Female"},
+    {"id": "765434", "first_name": "Joanne", "last_name": "Washington", "date_of_birth": "1989-03-29", "gender": "Female"},
+    {"id": "041408", "first_name": "Nathan", "last_name": "Howard", "date_of_birth": "2013-06-23", "gender": "Male"},
+    {"id": "190814", "first_name": "Alexandra", "last_name": "Barnett", "date_of_birth": "1970-08-11", "gender": "Female"}
+]
+
+@app.route('/foodallergyapi/patients', methods=['GET'])
+def get_patients():
+    return jsonify({'patients': patients_data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=5000)
